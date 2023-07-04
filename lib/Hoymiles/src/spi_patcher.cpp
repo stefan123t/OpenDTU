0ab0bb480f4436e4d98035ed87fbbb8c5c845be8
LennartF22 <18723691+LennartF22@users.noreply.github.com>
2023-07-04 21:29:01 +0200

    Share SPI3 between nRF and CMT

--- lib/Hoymiles/src/spi_patcher.cpp
+++ lib/Hoymiles/src/spi_patcher.cpp
@@ -11,22 +11,22 @@ spi_patcher::spi_patcher(spi_host_device_t host_device) :
 
 void spi_patcher::request(int8_t id, std::function<void(spi_host_device_t)> patch, std::function<void(spi_host_device_t)> unpatch)
 {
-    if (m_id & (1u << id)) {
+    /*if (m_id & (1u << id)) {
         m_id &= ~(1u << id);
         if (id == 0) {
             patch(SPI3_HOST);
         } else if (id == 1) {
             patch(SPI2_HOST);
         }
-    }
-    /*if (m_id != id) {
+    }*/
+    if (m_id != id) {
         if (m_unpatch.has_value()) {
             m_unpatch.value()(m_host_device);
         }
         patch(m_host_device);
         m_id = id;
         m_unpatch = unpatch;
-    }*/
+    }
 }
 
 void spi_patcher_inst_request(int8_t id, void (*patch)(spi_host_device_t), void (*unpatch)(spi_host_device_t))
